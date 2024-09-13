26a075fd6773a6e2061ed1b3bcb2e3b0ce7f6d1e
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-13 02:59:35 -0500

    error page

--- src/main/java/com/elementalcards/controller/CustomErrorController.java
+++ src/main/java/com/elementalcards/controller/CustomErrorController.java
@@ -1,14 +1,17 @@
 package com.elementalcards.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError() {
-        return "Ha ocurrido un error. Por favor, intenta nuevamente.";
+    public ModelAndView handleError() {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Página no encontrada");
+        return modelAndView;
     }
 }
