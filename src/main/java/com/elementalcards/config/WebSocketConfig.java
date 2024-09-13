3148f715c7c685151e3b3406d480b1588c4f1e72
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-12 23:06:57 -0500

    siii

--- src/main/java/com/elementalcards/config/WebSocketConfig.java
+++ src/main/java/com/elementalcards/config/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "https://juego-cartas-back.onrender.com", "https://juego-cartas-front.vercel.app").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
